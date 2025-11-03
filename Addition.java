32a6ec3048c5d5688207a9bcddaab672abeabd98
Anujith107 <rpanujith@gmail.com>
2025-11-03 19:23:16 +0530

    fix:Addition two numbers

---
+++ Addition.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Addition{
+    public static void main(String[] args){
+        Scanner ip=new Scanner(System.in);
+        int a=ip.nextInt();
+        int b=ip.nextInt();
+        int sum=a+b;
+        System.out.println("Sum:"+sum);
+    }
+}
