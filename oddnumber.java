bb7095d8e9f47272b46495d0497c0a879a30b0da
Anujith107 <rpanujith@gmail.com>
2025-11-06 10:11:55 +0530

    Fix:print od number

---
+++ oddnumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class oddnumber{
+    public static void main(String[] args) {
+        Scanner ip=new Scanner(System.in);
+        long n=ip.nextLong();
+        long sum=0;
+        ip.close();
+
+        for(int i=1;i<2*n;i++)
+        {
+            if(i%2==1)
+            sum=sum+i; //sum+=i;
+            
+        }
+        System.out.println(sum);
+    }
+}
