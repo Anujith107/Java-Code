1aa045654706a03c8c70bdaaabb969372b3a6087
Anujith107 <rpanujith@gmail.com>
2025-11-08 09:19:50 +0530

    Fix:Number occurance

---
+++ numberoccurance.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class numberoccurance {
+    public static void main(String[] args) {
+        Scanner ip=new Scanner(System.in);
+        int num=ip.nextInt();
+        int target=ip.nextInt();
+        int count=0;
+    
+        while(num!=0)
+        {
+            int dig=num%10;
+            if(dig==target)
+            {
+                count++;
+                num=num/10;
+            }
+        }
+    System.out.println(count);
+    ip.close();
+        
+        
+        
+    }
+    
+}
